bbb164c5c096c16e903514aae8d5febec9b16727
Blendhon <83319524+Blendhon@users.noreply.github.com>
2024-08-18 14:46:57 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
+// Autores: Blendhon e Rafael Oliveira
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define MAX_VERTICES 1000  // Máximo número de vértices suportados
+// Máximo número de vértices suportados
+#define MAX_VERTICES 1000  
 
 // Função para criar e inicializar a matriz de adjacência
 void initializeMatrix(int matrix[MAX_VERTICES][MAX_VERTICES], int numVertices) {
     for (int i = 0; i < numVertices; i++) {
         for (int j = 0; j < numVertices; j++) {
+            // Inicializa todas as células da matriz com 0
             matrix[i][j] = 0;
         }
     }
@@ -16,14 +20,16 @@ void initializeMatrix(int matrix[MAX_VERTICES][MAX_VERTICES], int numVertices) {
 // Função para adicionar uma aresta à matriz de adjacência
 void addEdge(int matrix[MAX_VERTICES][MAX_VERTICES], int src, int dest) {
     matrix[src - 1][dest - 1] = 1;
-    matrix[dest - 1][src - 1] = 1;  // Grafo não direcionado
+    matrix[dest - 1][src - 1] = 1;
+    // Grafo não direcionado
 }
 
-// Função auxiliar para DFS
+// Função para realizar uma Busca em Profundidade (DFS)
 void dfs(int matrix[MAX_VERTICES][MAX_VERTICES], int vertex, bool visited[], int numVertices) {
     visited[vertex] = true;
     for (int i = 0; i < numVertices; i++) {
         if (matrix[vertex][i] == 1 && !visited[i]) {
+            // Chama recursivamente a função dfs para o vértice adjacente
             dfs(matrix, i, visited, numVertices);
         }
     }
@@ -32,6 +38,7 @@ void dfs(int matrix[MAX_VERTICES][MAX_VERTICES], int vertex, bool visited[], int
 // Função auxiliar para contar o número de componentes
 int countComponents(int matrix[MAX_VERTICES][MAX_VERTICES], int numVertices) {
     bool visited[MAX_VERTICES];
+    // Inicializa todos os vértices como não visitados
     for (int i = 0; i < numVertices; i++) {
         visited[i] = false;
     }
@@ -39,6 +46,7 @@ int countComponents(int matrix[MAX_VERTICES][MAX_VERTICES], int numVertices) {
     int componentCount = 0;
 
     for (int i = 0; i < numVertices; i++) {
+        // Se o vértice ainda não foi visitado, então ele é uma nova componente
         if (!visited[i]) {
             dfs(matrix, i, visited, numVertices);
             componentCount++;
@@ -64,10 +72,12 @@ int main() {
         fclose(file);
         return 0;
     }
-    
+
+    // Cria a matriz de adjacência
     int matrix[MAX_VERTICES][MAX_VERTICES];
     initializeMatrix(matrix, N);
 
+    // Lê as arestas do arquivo e adiciona à matriz de adjacência
     for (int i = 0; i < M; i++) {
         int D, P;
         fscanf(file, "%d %d", &D, &P);
